a1f0dd484086ed9641b6fae57b16762deaac6270
TEZ TANMAY PANDAB <teztanmay5032555@gmail.com>
2026-01-04 16:27:13 +0530

    jan 4

--- hackerrank/runnerup.py
+++ hackerrank/runnerup.py
@@ -1,18 +1,18 @@
 scores = [2,3,3,5,4,4,6]
 
-# highest = float("-inf")
-# second = float("-inf")
+highest = float("-inf")
+second = float("-inf")
 
-# for i in scores:
-#     if i > highest:
-#         second = highest
-#         highest = i
+for i in scores:
+    if i > highest:
+        second = highest
+        highest = i
         
-#     else:
-#         if i < highest and i > second:
-#          second = i
+    else:
+        if i < highest and i > second:
+         second = i
 
-# print(second)
+print(second)
 
 
 # or
@@ -21,11 +21,3 @@ scores = [2,3,3,5,4,4,6]
 # set.sort()
 # runner_up = set[-2]
 # print(runner_up)
-
-
-scores = [2,3,6,6,5]
-
-remove_duplicates = list(set(scores))
-sort = remove_duplicates.sort()
-runner_up = sort[-2]
-print(runner_up)
