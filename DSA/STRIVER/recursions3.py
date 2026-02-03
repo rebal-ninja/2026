3bb3760e80424ba60af004a3d8693611fefbf331
TEZ TANMAY PANDAB <teztanmay5032555@gmail.com>
2026-02-03 15:02:51 +0530

    3/2

--- DSA/STRIVER/recursions3.py
+++ DSA/STRIVER/recursions3.py
@@ -86,3 +86,40 @@ def fibonacci(n):
     return(last+second_last)
 
 print(fibonacci(7))
+
+
+
+# ---------------- VISUAL DRY RUN ----------------
+# fibonacci(4)
+# = fibonacci(3) + fibonacci(2)
+#
+# fibonacci(3)
+# = fibonacci(2) + fibonacci(1)
+#
+# fibonacci(2)
+# = fibonacci(1) + fibonacci(0)
+#
+# fibonacci(1) → 1   (BASE CASE)
+# fibonacci(0) → 0   (BASE CASE)
+#
+# fibonacci(2) = 1 + 0 = 1
+#
+# fibonacci(1) → 1   (BASE CASE)
+#
+# fibonacci(3) = 1 + 1 = 2
+#
+# fibonacci(2)
+# = fibonacci(1) + fibonacci(0)
+# = 1 + 0 = 1
+#
+# fibonacci(4) = 2 + 1 = 3
+
+# f(4)
+# ├── f(3)
+# │   ├── f(2)
+# │   │   ├── f(1) → 1
+# │   │   └── f(0) → 0
+# │   └── f(1) → 1
+# └── f(2)
+#     ├── f(1) → 1
+#     └── f(0) → 0
