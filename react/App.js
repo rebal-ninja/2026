2c850e80cd5c8000fa42e70d51b080dffaa1c762
TEZ TANMAY PANDAB <teztanmay5032555@gmail.com>
2026-03-08 17:49:24 +0530

    Day

--- react/App.js
+++
@@ -1,5 +0,0 @@
-
-const h1ele=React.createElement(
-    h1,{},""
-)
-const ele= document.getElementById("root");
---
+++ react/my_first_reactproject/src/App.jsx
@@ -0,0 +1,25 @@
+// import React,{useState} from 'react'
+
+// const App = () => {
+    
+//     const [num, setnum] = useState(0)
+//   return (
+//     <div>
+//         <h1>NUMBER IS {num}</h1>
+//         <button onClick={()=>setnum(num+10)}>increment</button>
+//         <button onClick={()=>setnum(num-10)}>decriment</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+function App() {
+  return (
+    <h1 className="text-4xl text-red-500">
+      Tailwind working
+    </h1>
+  )
+}
+
+export default App
